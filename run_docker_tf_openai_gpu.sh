c960ccd1f762d3078d316845e25b91dd3cce6306
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2017-12-15 22:41:28 +0100

    Fix: using nvidia-docker to build so GPU is sure to work

--- run_docker_tf_openai_gpu.sh
+++ run_docker_tf_openai_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build --tag pascalwhoop/tf-openailab-gpu ./
+nvidia-docker build --tag pascalwhoop/tf-openailab-gpu ./
 nvidia-docker run --name jupyter -d -v /home/pascalwhoop/Documents/Code/Jupyter/Notebooks:/notebooks -e NB_UID=1000 -e NB_GID=1000 -e VNC_SERVER_PASSWORD=password -p 8888:8888 -p 6006:6006 -p 5900:5900  pascalwhoop/tf-openailab-gpu 
 echo "build succeeded, wait a bit for X to start, then getting the jupyter notebook URL with token"
 sleep 3
