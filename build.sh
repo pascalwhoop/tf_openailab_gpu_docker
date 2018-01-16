32be9b7ea3992257028f33bad5d6fe9905671929
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2018-01-17 00:24:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ if [ -z $1 ];
 	fi
   
 NOTEBOOKS_PATH=$1
-./kill_jupyter.sh
 nvidia-docker build --tag pascalwhoop/tf-openailab-gpu ./
 nvidia-docker run -d --name jupyter -v $NOTEBOOKS_PATH:/notebooks -e NB_UID=1000 -e NB_GID=1000 -e VNC_SERVER_PASSWORD=password -p 8888:8888 -p 6006:6006 -p 5900:5900  pascalwhoop/tf-openailab-gpu 
 echo "build succeeded, wait a bit for X to start, then getting the jupyter notebook URL with token"
